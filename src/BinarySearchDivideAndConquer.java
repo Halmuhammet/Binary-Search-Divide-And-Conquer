d684f072d40ca183fe6c25df3429479d395e69d7
Halmuhammet <hallysh9@gmail.com>
2025-05-22 17:19:09 +0500

    Refined the code and fixed a bug to correctly determine when a number is not in the array

--- src/BinarySearchDivideAndConquer.java
+++ src/BinarySearchDivideAndConquer.java
@@ -1,43 +1,36 @@
 /**
- * This algorithm is a Divide and Conquer implementation of Binary Search.
- * Base case is when the lowIndex is equal to highIndex
- * or when lowIndex is greater than highIndex (when target is not in the array).
- * Recursive case is when the function calls itself to apply the same algorithm on constricted array range.
+ * Description: This algorithm is a Divide and Conquer implementation of Binary Search.
+ * @author Halmuhammet Muhamedorazov
+ * @since 22.05.2025
+ * Note: Program return -1 when the target number is not in the array
+ *       And base case is when the lowIndex is equal to highIndex
  */
 
 class BinarySearchDivideAndConquer {
     public static void main(String[] args) {
-        int[] myArray = {4,5,6,7,8,9};
-        int foundIndex = binarySearch_divide_and_conquer(myArray, 0,myArray.length -1,0);
+        int[] myArray = {-57,-8,3,5,8,9,10};
+        int foundIndex = binarySearch_divide_and_conquer(myArray, 0,myArray.length -1,1);
         System.out.println(foundIndex);
     }
 
     public static int binarySearch_divide_and_conquer(int[] array, int lowIndex, int highIndex, int target){
-        //base case is when the target number is equal to the selected number
-        //now we need to think about how binary search works
-        //from what i remember it works by taking the selected number and
-        //comparing it against the target number to check weather
-        //it is equal to or greater or less than the target number.
 
-        //if the selected number is equal to the target number, then the algorithm terminates.
+        //if the selected number is equal to the target number or if it is not in the array, program returns -1.
         //if the selected number is less than the target number, then we set the lower index of array equal
         //to the index of the target number, and apply the same logic to the new array with constricted range
         //if the selected number is greater than the target number, then we set the upper index of array equal
         //to the index of the target number, and apply the same logic to the new array with constricted range.
 
-        // Now, to select a number in the array, we always divide the range of array by half and select resulting index
+        // When we are left with lowIndex == highIndex, it means there is only one element in the array
+        // We need to check whether this element is a target.
 
-        // Base case is when the array contains only 1 element, which would be the target
-
-
-
-        if (lowIndex > highIndex) {
-            return -1; // Base case: target not found
+        if (lowIndex == highIndex && array[lowIndex] != target) {
+            return -1; // Base case: target not in the array
         }
-        if(lowIndex == highIndex){
-            return lowIndex; // this is the target value
+        else if(lowIndex == highIndex && array[lowIndex] == target){
+            return lowIndex; // this is the index of target value
 
-            // Recursive case is when we miss the target value to go through the search process again.
+        // Recursive case is when we miss the target value to go through the search process again.
         }else {
             int updatedLowIndex = lowIndex;
             int updatedHighIndex = highIndex;
@@ -45,6 +38,7 @@ class BinarySearchDivideAndConquer {
             int selectedNumberIndex = (updatedLowIndex + updatedHighIndex) / 2;
             int selectedNumber = array[selectedNumberIndex];
 
+            // Update the lower and upper boundaries of array to narrow it down (divide)
             if (selectedNumber < target) {
                 updatedLowIndex = selectedNumberIndex + 1;
             }else if(selectedNumber > target){
@@ -53,7 +47,7 @@ class BinarySearchDivideAndConquer {
                 updatedHighIndex = updatedLowIndex;
             }
 
-
+            // Make a recursive call with narrowed array range
             return binarySearch_divide_and_conquer(array, updatedLowIndex, updatedHighIndex, target);
         }
     }
